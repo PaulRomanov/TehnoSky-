baa39ebc32240356ccbd26499da29d59a47e8771
PaulRomanov <ramosha@mail.ru>
2021-10-06 00:12:46 +0300

    add slider

--- belpost/src/App.tsx
+++ belpost/src/App.tsx
@@ -1,10 +1,14 @@
 import './styles.scss';
 import Header from './components/Header/Header';
+import Slider from './components/Slider/Slider';
+import Footer from './components/Footer/Footer';
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Slider />
+      <Footer />
     </>
   );
 };
