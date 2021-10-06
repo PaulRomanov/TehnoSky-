92fcf0ce76e0e4f7b4dca8c01f0a23a97380c925
PaulRomanov <ramosha@mail.ru>
2021-10-06 22:10:24 +0300

    add about page

---
+++ belpost/src/pages/About/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ContentAbout from '../../components/ContentAbout/ContentAbout'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import Slider from '../../components/Slider/Slider'
+
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <Slider />
+      <ContentAbout />
+      <Footer />
+    </div>
+  )
+}
+
+export default About;
