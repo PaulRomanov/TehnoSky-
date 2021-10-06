3d5ea3b0611b6c7bff726ebd06908c7717e2597b
Pavel <60575337+PaulRomanov@users.noreply.github.com>
2021-10-07 00:11:54 +0300

    add ready project

---
+++ belpost/src/components/ContentMainPage/ContentMainPage.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Button from '../Button/Button';
+import './contentmainPage.scss';
+
+const ContentMainPage = () => {
+  return (
+    <div className="contentMainPage_wrapper">
+      <div className="contentMainPage_content">
+        <NavLink to="/administrative">
+          <Button className="button" label="Административные процедуры" svgItem="administrative" />
+        </NavLink>
+        <NavLink to="/management">
+          <Button className="button" label="Руководство" svgItem="management" />
+        </NavLink>
+        <NavLink to="/appointmentSchedule">
+          <Button className="button" label="график приёма" svgItem="appointmentSchedule" />
+        </NavLink>
+        <NavLink to="/structure" className="last_link">
+          <Button className="button" label="Структура" svgItem="structure" />
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default ContentMainPage;
