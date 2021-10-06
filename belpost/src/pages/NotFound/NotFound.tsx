455bde39b881bb8b7197e3ca2ba765e5ec2e85e9
PaulRomanov <ramosha@mail.ru>
2021-10-06 22:09:13 +0300

    add page 404

---
+++ belpost/src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Slider from '../../components/Slider/Slider';
+import './notFound.scss';
+
+const NotFound = () => {
+  return (
+    <div>
+      <Header />
+      <Slider />
+      <div className="wrapper-404">
+      <nav>
+        <NavLink exact activeClassName="activ-link" className="shine-button btn-404" to="/">
+          Back to Home page
+        </NavLink>
+      </nav>
+      <img className="img-404" src="assets/404.png" alt="404" />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default NotFound;
